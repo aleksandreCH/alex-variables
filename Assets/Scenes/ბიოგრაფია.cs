14c93d8514d3e8b293a9199ac438ccd83cd9a385
aleksandreCH <162612528+aleksandreCH@users.noreply.github.com>
2024-11-27 23:18:37 +0400

    3

---
+++ Assets/Scenes/ბიოგრაფია.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ბიოგრაფია : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int myAge = 13;
+        int myName = "alex";
+        int mySuraname = "chogovadze";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
